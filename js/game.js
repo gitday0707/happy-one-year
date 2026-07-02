09d9948591f80dd977f99244ad18670c387c5187
BuiTrongThanh03 <thanhbuitrong03@gmail.com>
2026-07-03 04:33:40 +0700

    First commit

---
+++ js/game.js
@@ -0,0 +1,648 @@
+let currentQuestion = 0;
+let lives = 3;
+const recapMusic = document.getElementById("recapMusic");
+
+function typeWriter(element, text, speed, callback) {
+  let index = 0;
+
+  function type() {
+    if (index <= text.length) {
+      element.innerHTML =
+        text.substring(0, index) + '<span class="typing-cursor">|</span>';
+
+      const letterCard = document.querySelector(".letter-card");
+
+      letterCard.scrollTop = letterCard.scrollHeight;
+
+      index++;
+
+      setTimeout(type, speed);
+    } else {
+      element.innerHTML = text;
+
+      if (callback) callback();
+    }
+  }
+
+  type();
+}
+
+const tingSound = document.getElementById("tingSound");
+
+function startGame() {
+  bgMusic.volume = 0.4;
+
+  bgMusic.play().catch((err) => {
+    console.log("Không thể phát nhạc:", err);
+  });
+}
+
+const gameCard = document.querySelector(".game-card");
+
+function fadeOutGameCard() {
+  gameCard.classList.add("fade-out");
+}
+
+function fadeInGameCard() {
+  gameCard.classList.remove("fade-out");
+
+  gameCard.classList.add("fade-in");
+
+  setTimeout(() => {
+    gameCard.classList.remove("fade-in");
+  }, 600);
+}
+
+function loadQuestion() {
+  const gameCard = document.querySelector(".game-card");
+
+  gameCard.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+  const q = questions[currentQuestion];
+
+  // Progress
+  document.getElementById("progress").innerText =
+    `Câu ${currentQuestion + 1} / ${questions.length}`;
+
+  // Thanh progress
+  document.getElementById("progress-fill").style.width =
+    ((currentQuestion + 1) / questions.length) * 100 + "%";
+
+  // Nội dung câu hỏi
+  document.getElementById("question").innerText = q.question;
+
+  // Ảnh
+  const image = document.getElementById("question-image");
+
+  if (q.image) {
+    image.src = q.image;
+    image.style.display = "block";
+  } else {
+    image.style.display = "none";
+  }
+
+  // Đáp án
+  const answerContainer = document.getElementById("answer-container");
+
+  answerContainer.innerHTML = "";
+
+  q.options.forEach((option, index) => {
+    answerContainer.innerHTML += `
+            <label class="option">
+
+                <input
+                    type="radio"
+                    name="answer"
+                    value="${index}"
+                >
+
+                <span>${option}</span>
+
+            </label>
+        `;
+  });
+}
+
+function checkAnswer() {
+  const selected = document.querySelector('input[name="answer"]:checked');
+
+  if (!selected) {
+    alert("Mời em chọn đáp án hihi ❤️");
+
+    return;
+  }
+
+  const selectedOption = selected.closest(".option");
+
+  const userAnswer = Number(selected.value);
+
+  const correctAnswer = questions[currentQuestion].answer;
+
+  if (userAnswer === correctAnswer) {
+    selectedOption.classList.add("correct");
+
+    playConfetti();
+
+    setTimeout(() => {
+      correctAnswerEffect();
+    }, 800);
+  } else {
+    wrongAnswerEffect();
+  }
+}
+function correctAnswerEffect() {
+  console.log("correct");
+  fadeOutGameCard();
+
+  setTimeout(() => {
+    showMemory();
+  }, 1000);
+}
+function wrongAnswerEffect() {
+  showModal(
+    "💔 Sai rồi!",
+    "Mất 1 trái tim ❤️. Hãy nhớ lại thật kỹ nhé.",
+    function () {
+      lives--;
+
+      updateHeart();
+
+      if (lives <= 0) {
+        gameOver();
+      }
+    },
+  );
+}
+function updateHeart() {
+  const heartContainer = document.getElementById("heart-container");
+
+  heartContainer.classList.remove("heart-shake");
+
+  void heartContainer.offsetWidth;
+
+  heartContainer.classList.add("heart-shake");
+  let html = "";
+
+  for (let i = 0; i < lives; i++) {
+    html += "❤️ ";
+  }
+
+  for (let i = lives; i < 3; i++) {
+    html += "🤍 ";
+  }
+
+  document.getElementById("heart-container").innerHTML = html;
+}
+function gameOver() {
+  document.getElementById("gameOverModal").classList.remove("hidden");
+
+  document.getElementById("continueBtn").onclick = function () {
+    document.getElementById("gameOverModal").classList.add("hidden");
+
+    lives = 1;
+
+    updateHeart();
+  };
+}
+function finishGame() {
+  switchToRecapMusic();
+  showRecap();
+}
+function switchToRecapMusic() {
+  recapMusic.volume = 0;
+
+  recapMusic.currentTime = 0;
+
+  recapMusic.play();
+
+  const fade = setInterval(() => {
+    if (bgMusic.volume > 0) {
+      bgMusic.volume = Math.max(0, bgMusic.volume - 0.02);
+    }
+
+    if (recapMusic.volume < 0.45) {
+      recapMusic.volume = Math.min(0.45, recapMusic.volume + 0.02);
+    }
+
+    if (bgMusic.volume === 0 && recapMusic.volume === 0.45) {
+      bgMusic.pause();
+
+      clearInterval(fade);
+    }
+  }, 100);
+}
+function fadeOutMusic() {
+  const fade = setInterval(() => {
+    if (bgMusic.volume > 0.05) {
+      bgMusic.volume -= 0.05;
+    } else {
+      bgMusic.volume = 0;
+
+      bgMusic.pause();
+
+      clearInterval(fade);
+    }
+  }, 100);
+}
+function showModal(title, message, callback) {
+  document.getElementById("modalTitle").innerText = title;
+
+  document.getElementById("modalMessage").innerText = message;
+
+  document.getElementById("resultModal").classList.remove("hidden");
+
+  document.getElementById("modalBtn").onclick = function () {
+    document.getElementById("resultModal").classList.add("hidden");
+
+    if (callback) {
+      callback();
+    }
+  };
+}
+function showContinueButton() {
+  document.getElementById("memoryBtn").classList.add("show");
+}
+function showMemory() {
+  const memoryCard = document.querySelector(".memory-card");
+
+  memoryCard.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+  const memory = questions[currentQuestion].memory;
+
+  document.getElementById("memoryTitle").innerText = memory.title;
+
+  const image = document.getElementById("memoryImage");
+
+  if (memory.image) {
+    image.src = memory.image;
+
+    image.style.display = "block";
+  } else {
+    image.style.display = "none";
+  }
+
+  document.getElementById("memoryBtn").classList.remove("show");
+  typeWriter(document.getElementById("memoryStory"), memory.story, 50, () =>
+    showContinueButton(),
+  );
+
+  document.getElementById("memoryModal").classList.remove("hidden");
+}
+
+function playConfetti() {
+  confetti({
+    particleCount: 70,
+
+    spread: 70,
+
+    origin: {
+      x: 0.2,
+      y: 0.7,
+    },
+
+    colors: ["#ff5c99", "#ff85b5", "#ffd1e6", "#ffffff"],
+  });
+
+  confetti({
+    particleCount: 70,
+
+    spread: 70,
+
+    origin: {
+      x: 0.8,
+      y: 0.7,
+    },
+  });
+}
+
+updateHeart();
+document.getElementById("startJourneyBtn").onclick = function () {
+  const modal = document.getElementById("welcomeModal");
+
+  modal.classList.add("fade-out");
+
+  setTimeout(() => {
+    modal.classList.add("hidden");
+
+    document.querySelector(".game-card").classList.remove("hidden");
+
+    loadQuestion();
+
+    startGame();
+  }, 500);
+};
+
+document.getElementById("submitBtn").addEventListener("click", checkAnswer);
+
+document.getElementById("submitBtn").addEventListener("click", checkAnswer);
+document.getElementById("memoryBtn").onclick = function () {
+  document.getElementById("memoryModal").classList.add("hidden");
+
+  currentQuestion++;
+
+  if (currentQuestion >= questions.length) {
+    finishGame();
+
+    return;
+  }
+
+  fadeInGameCard();
+
+  loadQuestion();
+};
+
+// Recap part
+const recapImages = [
+  "/memories_img/1.JPG",
+  "/memories_img/2.JPG",
+  "/memories_img/3.JPG",
+  "/memories_img/4.JPG",
+  "/memories_img/5.JPG",
+  "/memories_img/6.JPG",
+  "/memories_img/7.JPG",
+  "/memories_img/8.jpg",
+  "/memories_img/9.JPG",
+  "/memories_img/10.JPG",
+  "/memories_img/11.JPG",
+  "/memories_img/12.JPG",
+  "/memories_img/13.JPG",
+  "/memories_img/14.jpg",
+  "/memories_img/15.jpg",
+  "/memories_img/16.JPG",
+  "/memories_img/17.JPG",
+  "/memories_img/18.jpg",
+  "/memories_img/19.JPG",
+  "/memories_img/21.JPG",
+  "/memories_img/22.jpg",
+  "/memories_img/23.jpg",
+  "/memories_img/24.JPG",
+  "/memories_img/25.JPG",
+  "/memories_img/26.JPEG",
+  "/memories_img/27.JPG",
+  "/memories_img/28.JPG",
+  "/memories_img/29.JPG",
+  "/memories_img/30.JPG",
+  "/memories_img/31.jpg",
+  "/memories_img/32.JPG",
+  "/memories_img/33.JPG",
+  "/memories_img/34.JPG",
+  "/memories_img/35.JPG",
+  "/memories_img/36.JPG",
+  "/memories_img/37.JPG",
+  "/memories_img/38.JPG",
+  "/memories_img/39.JPG",
+  "/memories_img/40.JPG",
+  "/memories_img/41.JPG",
+  "/memories_img/42.JPG",
+  "/memories_img/43.jpg",
+  "/memories_img/44.JPG",
+  "/memories_img/45.JPG",
+  "/memories_img/46.jpg",
+  "/memories_img/47.jpg",
+  "/memories_img/48.jpg",
+  "/memories_img/49.jpg",
+  "/memories_img/50.jpg",
+  "/memories_img/51.jpg",
+  "/memories_img/53.JPG",
+  "/memories_img/54.JPEG",
+  "/memories_img/55.JPG",
+  "/memories_img/56.JPG",
+  "/memories_img/57.jpg",
+  "/memories_img/58.JPG",
+  "/memories_img/59.JPG",
+  "/memories_img/60.jpg",
+  "/memories_img/61.jpg",
+  "/memories_img/62.jpg",
+  "/memories_img/64.jpg",
+  "/memories_img/65.jpg",
+  "/memories_img/66.JPG",
+  "/memories_img/67.JPG",
+  "/memories_img/68.JPG",
+  "/memories_img/69.JPG",
+  "/memories_img/70.JPG",
+  "/memories_img/71.JPG",
+  "/memories_img/72.PNG",
+  "/memories_img/73.JPG",
+  "/memories_img/74.JPG",
+  "/memories_img/75.jpg",
+  "/memories_img/76.JPG",
+  "/memories_img/77.JPG",
+  "/memories_img/78.JPG",
+  "/memories_img/79.JPG",
+  "/memories_img/80.JPG",
+  "/memories_img/81.JPG",
+  "/memories_img/82.JPG",
+  "/memories_img/83.JPG",
+  "/memories_img/84.jpg",
+  "/memories_img/85.PNG",
+  "/memories_img/86.JPG",
+  "/memories_img/87.JPEG",
+  "/memories_img/88.JPG",
+  "/memories_img/89.jpg",
+  "/memories_img/90.JPG",
+  "/memories_img/91.JPG",
+  "/memories_img/92.JPG",
+  "/memories_img/93.JPG",
+  "/memories_img/94.JPG",
+  "/memories_img/95.JPG",
+  "/memories_img/96.JPG",
+  "/memories_img/97.jpg",
+  "/memories_img/98.JPG",
+  "/memories_img/99.JPG",
+  "/memories_img/100.jpg",
+  "/memories_img/101.JPG",
+  "/memories_img/102.jpg",
+  "/memories_img/103.JPG",
+  "/memories_img/104.JPG",
+  "/memories_img/105.JPG",
+  "/memories_img/106.jpg",
+  "/memories_img/107.JPG",
+  "/memories_img/108.PNG",
+  "/memories_img/109.JPG",
+  "/memories_img/110.JPG",
+  "/memories_img/111.JPG",
+  "/memories_img/112.JPG",
+  "/memories_img/113.JPG",
+  "/memories_img/114.JPEG",
+  "/memories_img/115.JPG",
+  "/memories_img/116.JPG",
+  "/memories_img/117.JPG",
+  "/memories_img/118.JPG",
+  "/memories_img/119.JPG",
+  "/memories_img/120.JPG",
+  "/memories_img/121.JPG",
+  "/memories_img/122.JPG",
+  "/memories_img/123.JPG",
+  "/memories_img/124.jpg",
+  "/memories_img/125.JPG",
+  "/memories_img/126.JPG",
+  "/memories_img/127.JPG",
+  "/memories_img/128.jpg",
+  "/memories_img/129.JPG",
+  "/memories_img/130.JPG",
+  "/memories_img/131.JPG",
+  "/memories_img/132.JPG",
+  "/memories_img/133.jpg",
+  "/memories_img/134.JPG",
+  "/memories_img/135.JPG",
+  "/memories_img/136.JPG",
+  "/memories_img/137.JPG",
+  "/memories_img/138.JPG",
+  "/memories_img/139.JPG",
+  "/memories_img/140.JPG",
+  "/memories_img/141.JPG",
+  "/memories_img/142.JPG",
+  "/memories_img/143.JPG",
+  "/memories_img/144.JPG",
+  "/memories_img/145.JPG",
+  "/memories_img/146.JPG",
+  "/memories_img/147.JPG",
+  "/memories_img/148.JPG",
+  "/memories_img/149.JPG",
+  "/memories_img/150.JPG",
+  "/memories_img/151.JPG",
+  "/memories_img/152.JPG",
+  "/memories_img/153.JPG",
+  "/memories_img/154.JPG",
+  "/memories_img/155.JPG",
+  "/memories_img/156.JPG",
+  "/memories_img/157.JPG",
+  "/memories_img/158.JPG",
+  "/memories_img/159.JPG",
+  "/memories_img/160.JPG",
+  "/memories_img/161.JPG",
+  "/memories_img/162.JPG",
+  "/memories_img/163.jpg",
+  "/memories_img/164.JPG",
+  "/memories_img/165.JPG",
+  "/memories_img/166.JPG",
+  "/memories_img/167.jpg",
+  "/memories_img/168.JPG",
+];
+
+let recapIndex = 0;
+let recapInterval;
+function changeRecapImage() {
+  const image = document.getElementById("recapImage");
+
+  image.classList.add("fade");
+
+  setTimeout(() => {
+    image.onload = () => {
+      image.classList.remove("fade");
+    };
+
+    image.src = recapImages[recapIndex];
+  }, 100);
+}
+function showRecap() {
+  clearInterval(recapInterval);
+
+  recapIndex = 0;
+
+  document.getElementById("recapModal").classList.remove("hidden");
+
+  changeRecapImage();
+
+  recapInterval = setInterval(() => {
+    recapIndex++;
+
+    if (recapIndex >= recapImages.length) {
+      clearInterval(recapInterval);
+
+      setTimeout(() => {
+        document.getElementById("recapModal").classList.add("hidden");
+
+        showLetter();
+      }, 3000);
+
+      return;
+    }
+
+    changeRecapImage();
+  }, 800);
+}
+const letter = `
+Em à,
+Thời gian trôi nhanh thật, 2 năm quen em và 1 năm yêu em,
+Thời gian qua chúng ta trải qua nhiều chuyện, vui cũng có mà buồn cũng có, đặc biệt là cũng có nhiều lúc cãi vã giận dỗi.
+Thế nhưng mà dù trải qua truyện gì thì chúng mình cũng học cách hỏi han, tha thứ cho nhau để rồi thấu hiểu nhau hơn. 
+Sau một năm, mình cũng trải qua được rất nhiều kỉ niệm, đây là một món quà nhỏ anh làm để chúng mình cùng nhìn lại những kỷ niệm ấy.
+Từng câu hỏi, từng bức ảnh, từng kỉ niệm...
+đều là những mảnh ghép của cuộc tình chúng mình trong một năm vừa qua
+Có những chuyện em nhớ ngay,
+Có những chuyện em phải suy nghĩ một lúc mới nhớ ra được,
+Và có những thứ nhỏ nhặt em chưa để ý,
+Nhưng mà điều đó chẳng quan trọng.
+
+Quan trọng là ....
+chúng mình đã trải qua chúng cùng nhau sau tất cả.
+
+Một năm trước,
+Anh không nghĩ sẽ có một ngày bình thường thành một ngày đặc biệt như vậy
+Cảm ơn em vì đã xuất hiện,
+Cảm ơn em vì đã luôn ở bên anh,
+lắng nghe anh,
+động viên anh,
+và cùng anh trải qua những ngày vui ngày buồn và những lúc mệt mỏi.
+
+Có những mình giận nhau,
+những lúc mình hiểu lầm,
+và cũng có lúc hai đứa cùng cứng đầu cái tôi cao.
+Nhưng rồi...
+mình vẫn chọn ở lại cùng nhau giải quyết vấn đề và làm hòa với nhau
+
+Cảm ơn em đã cùng anh vẽ lên những kỉ niệm có đẹp và cũng có buồn suốt một năm qua
+Mỗi bức ảnh vừa rồi
+không chỉ là một tấm hình
+mà còn là một ngày mình cùng ở bên nhau, cười nói rất nhiều cùng nhau, 
+là một món quà,
+là một cái nắm tay,
+là một cái ôm,
+là một cái thơm má,
+thậm chí là một nụ hôn,
+hay đơn giản chỉ là...
+một khoảnh khắc có em ở bên cạnh
+
+Anh không biết tương lai sẽ có những gì đang chờ phía trước 
+Nhưng mà anh tin rằng...
+Anh sẽ luôn đồng hành cùng em để vẽ nên thật nhiều kỷ niệm trong cuốn sách về cuộc tình chúng mình.
+Để rồi một ngày nào đó,
+website này không chỉ có hơn 160 ảnh recap
+Mà sẽ là hàng vài trăm,
+vài nghìn,
+hay thậm chí là cả cuộc đời đầy kí ức đẹp.
+(Mặc dù mới gần 200 ảnh mà anh đã hơi mỏi tay nha bé 🥰)
+
+Nếu sau này em có vào lại website này
+Anh hy vọng em sẽ mỉm cười 
+và nhớ rằng ...
+
+Đã có một chàng trai (Chòn em)
+thức đêm và dành nhiều thời gian 
+để viết từng dòng code,
+chỉnh từng hiệu ứng,
+chọn từng bài nhạc,
+chỉ mong em có thể vui hơn, bất ngờ hơn một chút khi xem nó
+
+Bởi vì...
+Em là món quà tuyệt nhất
+mà anh đã có được trong năm vừa rồi,
+anh hứa sẽ giữ gìn và chăm sóc món quà này cẩn thận.
+
+Happy One Year, em bé nhé ❤️
+Cảm ơn em vì đã đến.
+Cảm ơn em vì đã ở lại.
+Và cảm ơn em vì đã cùng anh viết nên chương đầu tiên
+của câu chuyện tình yêu giành riêng cho "chúng mình".
+
+Anh yêu em rất nhiều.
+Bã chã của anh :)))
+❤️
+`;
+
+function showLetter() {
+  document.getElementById("letterModal").classList.remove("hidden");
+
+  typeWriter(document.getElementById("letterContent"), letter, 50, () => {
+    document.getElementById("finishBtn").classList.add("show");
+  });
+}
+
+document.getElementById("finishBtn").onclick = function () {
+  confetti({
+    particleCount: 250,
+
+    spread: 120,
+  });
+  showEnding();
+};
+function showEnding() {
+  document.getElementById("letterModal").classList.add("hidden");
+
+  document.getElementById("endingScreen").classList.remove("hidden");
+}
---
+++ js/questions.js
@@ -0,0 +1,263 @@
+const questions = [
+{
+    question:"Lần đầu mình nhắn tin là khi nào?",
+    image: "https://i.redd.it/rku3tl5b9mcb1.jpg",
+    options:[
+        "20/07/2025",
+        "21/07/2025",
+        "22/07/2025",
+        "24/07/2025"
+    ],
+    answer:1, 
+    memory: {
+
+        title: "Tin nhắn đầu tiên",
+
+        image: "/images/nhantinlandau.jpg",
+
+        story: "A ơi, em đki link r nma bấm vào link lớp thì nó lỗi ạ:) A add e vào với. HEHE"
+
+    }
+},
+{
+    question:"Lần đâu đi ăn chung là ăn gì?",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIJIKgZwjS0xs38Qpj-LudqUArbNSvkN86OWuWxG7FTA&s=10",
+    options:[
+        "Mỳ cay",
+        "Gà",
+        "Uống nước",
+        "Xiên bẩn"
+    ],
+    answer:1, 
+    memory: {
+
+        title: "Lần đầu đi ăn chung",
+
+        image: "/images/bkz.jpg",
+
+        story: "Người hoa mắt tụt huyết áp cùng người tóc bết đang ủ vào quán ăn gà."
+
+    }
+},
+{
+    question:"Ngày yêu nhau là khi nào?",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcnZHO7i__5e33Sa0j9eCdqFBlu1Kk5JdQDAWLO7FRfP7dZabpyeAUBvfW&s=10",
+    options:[
+        "07/07",
+        "13/07",
+        "06/07",
+        "08/07"
+    ],
+    answer:0,
+    memory: {
+
+        title: "Ngày yêu nhau",
+
+        image: "/images/theone.JPG",
+
+        story: "Đêm hôm ý thức cũng khuya á, em còn ngủ với mẹ may mà không đứng tim ra đấy"
+
+    }
+},
+{
+    question:"Ngày mình tỏ tình là bao giờ?",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQhZXQcRwMAKol5tcSlrxZXgX0JRHvaNwRGn-n3hyNDvfTHoImzzgPsdU&s=10",
+    options:[
+        "07/07",
+        "08/07",
+        "13/07",
+        "14/07"
+    ],
+    answer:2, 
+    memory: {
+
+        title: "Ngày tỏ tình",
+
+        image: "/images/ngaytotinh.jpg",
+
+        story: "Lời tỏ tình văn chương đủ mở bài thân bài kết bài, mặc dù em cãi hơi ngang nhưng mà a chịu đựng được :)), chưa bao giờ thấy gian nan như hôm ý."
+
+    }
+}, 
+{
+    question:"Hộp quà lúc tỏ tình được buộc dây màu gì?",
+    image: "https://api.muji.com.vn/media/catalog/product/cache/2e9290695da361a7d6192a4c8c689807/4/5/4550584925177_org.jpg",
+    options:[
+        "Xanh",
+        "Hồng",
+        "Trắng",
+        "Đỏ"
+    ],
+    answer:3, 
+    memory: {
+
+        title: "Hộp quà tỏ tình",
+
+        image: "/images/hopqua.JPG",
+
+        story: "Hộp giấy nâu, màu đỏ nhá. Người ta chọn cả buổi trưa đấy."
+
+    }
+}, 
+{
+    question:"Lúc mở quà thì bị rơi đồ gì?",
+    image: "/images/doroi.jpg",
+    options:[
+        "Vòng tay",
+        "Vòng cổ",
+        "Buộc tóc",
+        "Kẹp tóc"
+    ],
+    answer:0,
+    memory: {
+
+        title: "Đồ bị rơi",
+
+        image: "/images/vongtay.PNG",
+
+        story: "Đánh rơi mất cái vòng, còn lừa người ta không lên tìm để tự lên tìm nữa :)) Nhưng mà mất cũng may giờ có vòng tay mới"
+
+    }
+}, 
+{
+    question:"Sinh nhật em anh tặng em quà gì?",
+    image: "/images/sinhnhat.JPG",
+    options:[
+        "Cốc",
+        "JellyCat",
+        "Thỏ Zootopia",
+        "Móc khóa"
+    ],
+    answer:1,
+    memory: {
+
+        title: "Quà sinh nhật",
+
+        image: "/images/jellycat.JPG",
+
+        story: "Chọn quà này cũng lâu á, có cả video em unbox mà. Hôm ý bố đã đoán ra là tặng em rồi, mỗi tội mùi hơi coolfresh."
+
+    }
+},
+
+{
+    question:"Giáng sinh anh định tặng em cái gì đầu tiên?",
+    image: "/images/giangsinh.JPG",
+    options:[
+        "Cốc",
+        "Không tặng gì",
+        "Sừng",
+        "Móc khóa"
+    ],
+    answer:2,
+    memory: {
+
+        title: "Quà lúc đầu",
+
+        image: "/images/quagiangsinh.jpg",
+
+        story: "Lúc đầu a định tặng cái sừng đấy, trêu e xong gặp luôn ở quán mới hay chứ."
+
+    }
+},
+{
+    question:"Hộp quà valentine có bao nhiêu viên sicula?",
+    image: "/images/vlt.jpg",
+    options:[
+        "8",
+        "9",
+        "10",
+        "11"
+    ],
+    answer:2, 
+    memory: {
+
+        title: "Hộp sicula",
+
+        image: "/images/sicula.JPG",
+
+        story: "Đếm xem mấy viên :)) Hôm ý hơi ứ ừ đẹ. Với cả ăn sicuala hơi lâu nhá"
+
+    }
+},
+
+{
+    question:"Lần đầu hôn má là hôm nào?",
+    image: "/images/thomma1.JPG",
+    options:[
+        "1/12",
+        "8/12",
+        "9/12",
+        "22/12"
+    ],
+    answer:1, 
+    memory: {
+
+        title: "Hun má",
+
+        image: "/images/honma.JPG",
+
+        story: "Phần thưởng hơi rườm rà, cứ đợi nhân viên nó không nhìn. Chiều hôm ý còn đi ăn tacos."
+
+    }
+},
+{
+    question:"Lần đầu hôn môi là khi nào?",
+    image: "/images/honmoi.JPG",
+    options:[
+        "8/12",
+        "9/12",
+        "22/12",
+        "29/12"
+    ],
+    answer:3, 
+    memory: {
+
+        title: "Hun môi",
+
+        image: "/images/honmoi1.JPG",
+
+        story: "Sáng đi thi còn chiều hẹn thì phải. Tôi yêu đoán các nước hê hê. Lần đầu mà mắt cứ mở thô lố ra."
+
+    }
+},
+{
+    question:"Lần đầu chụp ảnh photobooth là khi nào?",
+    image: "/images/ptb1.JPG",
+    options:[
+        "24/03/2026",
+        "25/03/2026",
+        "26/03/2026",
+        "27/03/2026"
+    ],
+    answer:1, 
+    memory: {
+
+        title: "PHOTOBOOTH",
+
+        image: "/images/ptb2.PNG",
+
+        story: "Đi xem phim ma, được ôm hơi nhìu, hơi bị thít hehe"
+
+    }
+},
+
+{
+    question:"E có muốn xem phần tiếp theo không?",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6nESIxp0Pqs4rsylVpVfoLExyjj7AWy9C_Qvob-18oQ&s=10",
+    options:[
+        "Có",
+        "Không",
+    ],
+    answer:0,
+    memory: {
+
+        title: "Chuyển sang mục tiếp theo",
+
+        image: "/images/nextpart.jpg",
+
+        story: "Sang phần tiếp theo xem sao"
+
+    }
+}
+];
